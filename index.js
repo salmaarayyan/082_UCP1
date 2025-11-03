ccde711add13486a184779277166bcc89e4aa9c2
salmaarayyan <salmaarayyan@gmail.com>
2025-11-03 08:35:17 +0700

    Menambahkan middleware untuk membaca data JSON dan form (POST body) di index.js kemudian menjalankan server di port 3000

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express();
 const PORT = 3000;
 
-// Import folder models
 const db = require("./models");
 
+// Middleware untuk membaca data JSON dan form (POST body)
+app.use(express.json());
+app.use(express.urlencoded({ 
+    extended: false
+}));
+
+// Jalankan server di port 3000
+app.listen(PORT, () => {
+    console.log('Server started on port 3000');
+})
