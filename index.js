cd28ece4c7e53e88c00c0e426660e12ce8a7b861
salmaarayyan <salmaarayyan@gmail.com>
2025-11-03 08:36:15 +0700

    Menambahkan sinkronisasi model ke database sebelum menjalankan server

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const PORT = 3000;
 
 const db = require("./models");
 
-// Middleware untuk membaca data JSON dan form (POST body)
 app.use(express.json());
 app.use(express.urlencoded({ 
     extended: false
 }));
 
-// Jalankan server di port 3000
 app.listen(PORT, () => {
     console.log('Server started on port 3000');
 })
+
+db.sequelize.sync()
+    .then((result) => {
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
